257f4d4d008bcf335f87a68cbf14ae1dc1f37467
lkarjun <lk.arjun@hotmail.com>
2022-05-29 20:16:36 +0530

    Updates

--- Models/model/__init__.py
+++ Models/model/__init__.py
@@ -7,4 +7,6 @@ TRAIN = "heartdisease_indicator_train.csv"
 VALIDATION = "heartdisease_indicator_t.csv"
 
 with open(MODEL_DIR/'VERSION.json', 'r') as version:
-    __version__ = json.load(version)['Version']
+    __meta__ = json.load(version)
+    __version__ = __meta__['Version']
+    __tag__ = __meta__['Tag']
