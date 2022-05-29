ae4b59dfbc4bf41d9aa45524dc83de1560b87cf6
lkarjun <lk.arjun@hotmail.com>
2022-05-29 09:27:44 +0530

    Update model loading

--- Models/model/inference.py
+++ Models/model/inference.py
@@ -13,9 +13,10 @@ class Inference:
         self.c = {0: 'No', 1: 'Yes'}
         self.model = self._load_model()
         self.preprocess = PreProcess()
+        self.failed = False
 
     def predict(self, data: DataFrame):
-        if self.model is None:
+        if self.model == None:
             return {'Error': 'Failed to load model'}
         data = self.preprocess.transform(data)
         pred = self.model.predict_proba(data)
@@ -23,17 +24,18 @@ class Inference:
         accurate_prediciton = self.c[np.argmax(pred)]
         prediction = {"__version__": self.version,
                       "predictions": {'No': pred[0], 'Yes': pred[1]},
-                      "accurate_prediction": accurate_prediciton}
+                      "label": accurate_prediciton}
         return prediction
 
     def _load_model(self):
         try:
-            self.model = joblib.load(MODEL_DIR/self.model_name)
-            return
+            model = joblib.load(MODEL_DIR/self.model_name)
+            return model
         except Exception:
             status = self._pull_model()
-            if status and (self.retry_state == 5):
+            if status and (self.retry_state >= 5):
                 print(f"Downloading model -> Retrying {self.retry_state}")
+                self.failed = True
                 return
             self._load_model()
             self.retry_state += 1
