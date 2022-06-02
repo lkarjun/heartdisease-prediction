cdaaa098fa783bd69b5ea65902460432b162e737
LalKrishna Arjun <58617251+lkarjun@users.noreply.github.com>
2022-06-02 18:53:39 +0530

    Update about.py

--- App/about.py
+++ App/about.py
@@ -6,8 +6,6 @@ from pathlib import Path
 readme_file = Path(__file__).resolve().parent.parent/'readme.md'
 
 def main():
-    col,_ = st.columns(2)
-    with col:
-        st.markdown(readme_file.read_text('utf-8'), True)
+    st.markdown(readme_file.read_text('utf-8'), True)
 
 
