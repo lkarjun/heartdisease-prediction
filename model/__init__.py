5051567ce3ce52a97ee8e79ed476b511bc170123
lkarjun <lk.arjun@hotmail.com>
2022-06-03 13:11:06 +0530

    Updates

--- model/__init__.py
+++ model/__init__.py
@@ -3,10 +3,13 @@ from pathlib import Path
 
 MODEL_DIR = Path(__file__).resolve().parent
 DATASET_DIR = Path(__file__).resolve().parent.parent.parent/'Dataset'
+TRACKING_URI = Path(__file__).resolve().parent/'model_tracking'
 TRAIN = "heartdisease_indicator_train.csv"
 VALIDATION = "heartdisease_indicator_t.csv"
 
-with open(MODEL_DIR/'VERSION.json', 'r') as version:
+with open(MODEL_DIR/'info.json', 'r') as version:
     __meta__ = json.load(version)
-    __version__ = __meta__['Version']
-    __tag__ = __meta__['Tag']
+    __version__ = __meta__['version']
+    __tag__ = __meta__['tag']
+    __latest_run_id__ = __meta__['run_id']
+    __experiment_id__ = __meta__['experiment_id']
