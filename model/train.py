dac8104e0c7c33e5c4044010ce08001d7899459a
lkarjun <lk.arjun@hotmail.com>
2022-06-07 13:18:48 +0530

    Model tags fixes

--- model/train.py
+++ model/train.py
@@ -91,7 +91,9 @@ def train():
                    'model': model,
                    'tags': {'estimator_name': type(model).__name__,
                             'model_tag': "BaseLine",
-                            'model_version': "0.1.0"
+                            'model_version': "0.1.1",
+                            'model_type': "Ensemble",
+                            'trained_from': 'Github CI CD'
                             }
                    }
 
