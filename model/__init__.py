3d804082ab48d78780a2f7da8a85b8fbaaacb904
lkarjun <lk.arjun@hotmail.com>
2022-06-07 14:55:26 +0530

    Fixes

--- model/__init__.py
+++ model/__init__.py
@@ -6,11 +6,9 @@ DATASET_DIR = Path(__file__).resolve().parent.parent.parent/'dataset'
 TRACKING_URI = Path(__file__).resolve().parent/'model_tracking'
 TRAIN = "heartdisease_indicator_train.csv"
 VALIDATION = "heartdisease_indicator_t.csv"
+__experiment_id__ = "0"
 
-with open(MODEL_DIR/'info.json', 'r') as version:
-    __meta__ = json.load(version)
-    __version__ = __meta__['version']
-    __tag__ = __meta__['tag']
-    __latest_run_id__ = __meta__['run_id']
-    __experiment_id__ = __meta__['experiment_id']
-    __updated_time__ = __meta__['updated_time']
+def get_infos():
+    with open(MODEL_DIR/'info.json', 'r') as version:
+        data = json.load(version)
+    return data
