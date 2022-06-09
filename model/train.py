12fc42abd2b03ede3715aae0f77668edd2dc98f3
lkarjun <lk.arjun@hotmail.com>
2022-06-09 14:18:43 +0530

    Files updated

--- model/train.py
+++ model/train.py
@@ -1,57 +1,17 @@
-from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score, f1_score
-from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score, accuracy_score, \
+                            precision_score, recall_score, \
+                            f1_score
 from preprocess import PreProcess
-import matplotlib.pyplot as plt
+from utils import load_data, save_fig, save_metrics, save_preprocess
 from pathlib import Path
-import pandas as pd
-import pickle
 import yaml
 
-METRICS = {'Dataset': ['Training', 'Testing']}
+CONFIG_PATH = Path(__file__).resolve().parent/'configs'
 
-## STATIC PATHS
-TARGET = "HeartDisease"
-ASSEST_DIR = Path(__file__).resolve().parent/'resources'
-DATASET_DIR = Path(__file__).resolve().parent.parent/'dataset'
-
-ASSEST_DIR.mkdir(exist_ok=True)
-
-def get_data():
-
-    ## DATALOADING
-    TRAIN_TEST = pd.read_csv(DATASET_DIR/'heartdisease_indicator_train.csv')
-    VALID = pd.read_csv(DATASET_DIR/'heartdisease_indicator_test.csv')
-    print("\n\nLoad data...✅")
-
-    ## DATA PREPROCESS
-    TRAIN_TEST['GenHealth'] = pd.Categorical(TRAIN_TEST.GenHealth, 
-                                            categories=["Poor","Fair","Good",
-                                                        "Very good","Excellent"],
-                                            ordered=True)
-    X_train, Y_train = TRAIN_TEST.drop(TARGET, axis = 1), TRAIN_TEST[TARGET]
-    X_test, Y_test = VALID.drop(TARGET, axis = 1), VALID[TARGET]
-    return X_train, Y_train, X_test, Y_test
-
-def run_training(model_parmas: dict, tags: dict):
-
-    X_train, Y_train, X_test, Y_test = get_data()
-    preprocess = PreProcess()
-    X_train, Y_train = preprocess.transform(X_train, Y_train)
-    X_test, Y_test = preprocess.transform(X_test, Y_test)
-
-    print("Data Preprocess...✅")
-
-    ## MODELING
-    model = RandomForestClassifier(**model_parmas)
-    model.fit(X_train, Y_train)
-    print("Model Fit...✅")
-    
-    ## PREDICTION
-    Y_train_preds = model.predict(X_train)
-    Y_test_preds = model.predict(X_test)
-    
-    ## METRICS
+# Change if metrics need to be updated
+def cal_matrics(Y_train, Y_train_preds, Y_test, Y_test_preds):
+    METRICS = {'Dataset': ['Training', 'Testing']}
 
     METRICS['Accuracy']= accuracy_train_test = [round(accuracy_score(Y_train, Y_train_preds), 2), 
                                                 round(accuracy_score(Y_test, Y_test_preds), 2)]
@@ -64,37 +24,49 @@ def run_training(model_parmas: dict, tags: dict):
     METRICS['ROU-AUC-SCORE'] = rocauc_train_test = [round(roc_auc_score(Y_train, Y_train_preds), 2), 
                                                     round(roc_auc_score(Y_test, Y_test_preds), 2)]
 
+    metrics = {'accuracy': accuracy_train_test[1],
+               'precision': precision_train_test[1],
+               'recall': recall_train_test[1],
+               'f1_score': f1_score_train_test[1],
+               'roc_auc': rocauc_train_test[1]}
+    return metrics, METRICS
 
-    ## VALIDATION
-    ConfusionMatrixDisplay.from_estimator(model, X_test, Y_test)
+def run_training(model_parmas: dict, tags: dict, reg_model_name: str, compare_latest:bool):
+    X_train, Y_train, X_test, Y_test = load_data()
+    preprocess = PreProcess()
+    X_train, Y_train = preprocess.transform(X_train, Y_train)
+    X_test, Y_test = preprocess.transform(X_test, Y_test)
+    print("Data Preprocess...✅")
 
-    ## SAVE FIG AND METRICS
-    plt.savefig(ASSEST_DIR/'Confusion-Matrix.png')
-    pd.DataFrame(METRICS).to_html(ASSEST_DIR/'metrics.md', index = False)
-    print("Save Metrics...✅")
+    ## MODELING
+    model = RandomForestClassifier(**model_parmas)
+    model.fit(X_train, Y_train)
+    print("Model Fit...✅")
+    
+    ## PREDICTION
+    Y_train_preds = model.predict(X_train)
+    Y_test_preds = model.predict(X_test)
+    
+    metrics, METRICS = cal_matrics(Y_train, Y_train_preds, Y_test, Y_test_preds)
 
-    ## Save Preprocessor
-    with open(ASSEST_DIR/"preprocessor.pkl", "wb") as file:
-        pickle.dump(preprocess, file)
-        print("Save Preprocessor...✅")
+    ## SAVE FIGs, METRICS & Preprocessor
+    save_fig(model, X_test, Y_test, reg_model_name, compare_latest)
+    save_preprocess(preprocess)
+    save_metrics(METRICS)
 
-    return_data = {'metrics': {'accuracy': accuracy_train_test[1],
-                               'precision': precision_train_test[1],
-                               'recall': recall_train_test[1],
-                               'f1_score': f1_score_train_test[1],
-                               'roc_auc': rocauc_train_test[1]},
-                   'params': model.get_params(),
-                   'model': model,
-                   'tags': tags | {'estimator_name': type(model).__name__}
-                   }
+    data = {'metrics': metrics, 'params': model.get_params(), 'model': model,
+            'tags': tags | {'estimator_name': type(model).__name__}}
 
-    return return_data
+    return data
 
-
-def train():
-    with open(ASSEST_DIR.parent/'configs/training_config.yaml') as file:
+def train(reg_model_name, compare_latest = False):
+    with open(CONFIG_PATH/'training_config.yaml') as file:
         data = yaml.load(file, yaml.FullLoader)
-    return run_training(**data)
+    return run_training(**data, reg_model_name=reg_model_name, compare_latest=compare_latest)
 
 if __name__ == "__main__":
-    train()
+
+    with open(CONFIG_PATH/'track_training_config.yaml') as file:
+        data = yaml.load(file, yaml.FullLoader)
+
+    train(data['registered_model_name'], True)
