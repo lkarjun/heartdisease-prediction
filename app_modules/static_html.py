0a46b6e6a8f2ce27489e39742391b3650f977599
lkarjun <lk.arjun@hotmail.com>
2022-06-10 14:31:28 +0530

    Files updated

--- app_modules/static_html.py
+++ app_modules/static_html.py
@@ -49,7 +49,7 @@ home_version_display = """
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 <div style='font-size: 18px;'>
     <div class='text-center'>
-        <span class="badge text-bg-info">{{ tag }} v{{ v }}</span>
+        <span class="badge text-bg-info">{{ tag }} v{{ v }}.0</span>
     </div>
 </div>
 """
